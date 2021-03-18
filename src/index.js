9e7f88a6e7d994f91c345380a84427a9263101d3
Dempsey <dempsey.rhian.neal@gmail.com>
2021-03-18 16:23:30 +0000

    added Provider import and brought getstate() out of function. Added React.StrictMode tags wrapping aroung Provider that grabs the store. Deleted render() and subscribe.store(render), these are not needed.

--- src/index.js
+++ src/index.js
@@ -2,46 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from '../src/data/store';
+import store from './data/store';
+import { Provider } from "react-redux";
 
 
+let state = store.getState();
 
-const render = () => {
-  let state = store.getState();
 
 ReactDOM.render(
   <React.StrictMode>
-        {/* we update subscribe to call the ReactDOM.render
-        method whenever the state changes */}
-        <App 
-        player1 = { state.player1 }
-        handleIncrementPlayer1={ () => store.dispatch({ type: "INCREMENTPLAYER1" })} 
-        player2 = {state.player2} 
-        handleIncrementPlayer2={ () => store.dispatch({ type: "INCREMENTPLAYER2" })}
-        handleReset={ () => store.dispatch({ type: "RESET" })}
-        serving = { state.serving }
-        winner = { state.winner }
-        />
-  </React.StrictMode>,
+    <Provider store= { store }>
+        
+            <App player1 = { state.player1 }
+                  handleIncrementPlayer1={ () => 
+                    
+                  store.dispatch({ type: "INCREMENTPLAYER1" })} 
+                  player2 = {state.player2} 
+                  
+                  handleIncrementPlayer2={ () => 
+                  store.dispatch({ type: "INCREMENTPLAYER2" })}
+                  
+                  handleReset={ () => store.dispatch({ type: "RESET" })}
+                  
+                  serving = { state.serving }
+             />
+        </Provider>
+      </React.StrictMode>,
   document.getElementById('root')
   );
-};
-
-
-store.subscribe(render); /* render when state changes */
-
-
-// Why don't we need to dispatch the action? We are not using subscribe with actions, so don't really need the dispatch.
-// store.dispatch({ type: "INCREMENTPLAYER1" });
-// store.dispatch({ type: "INCREMENTPLAYER2" });
-// store.dispatch({ type: "RESET" });
-
-
-render(); /*render when page loads using initial state */
 
 
 
 
-// dispatch, dispatches an action
-// createStore is the Redux part of it all
-//
+
+
+
